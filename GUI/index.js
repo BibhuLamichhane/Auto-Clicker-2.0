1e38640fc3ab3895aed54dc03d2bee36eaea370a
BibhuLamichhane <lamichhanebibhu0@gmail.com>
2020-12-01 23:59:18 +0545

    minor changes

--- GUI/index.js
+++ GUI/index.js
@@ -1,4 +1,4 @@
-function keyboardActive() {
+let keyboardActive = () => {
     let kTab = document.getElementById('keyboard')
     kTab.className = 'activeTab'
     let mTab = document.getElementById('mouse')
@@ -9,7 +9,7 @@ function keyboardActive() {
     m.className = 'hideContents'
 }
 
-function mouseActive() {
+let mouseActive = () => {
     let mTab = document.getElementById('mouse')
     mTab.className = 'activeTab'
     let kTab = document.getElementById('keyboard')
@@ -20,6 +20,31 @@ function mouseActive() {
     k.className = 'hideContents'
 }
 
+let activeSubTab = (evt) =>{
+    let elem = document.getElementById(evt.currentTarget.val)
+    elem.className = 'activeSubTab'
+
+}
+
 document.getElementById('keyboard').addEventListener('click', keyboardActive)
 document.getElementById('mouse').addEventListener('click', mouseActive)
+
+let alpha = document.getElementById('alphabets')
+let number = document.getElementById('numbers')
+let symbol = document.getElementById('symbols')
+let spKeys = document.getElementById('specialKeys')
+let customText = document.getElementById('customText')
+
+alpha.addEventListener('click', activeSubTab)
+number.addEventListener('click', activeSubTab)
+symbol.addEventListener('click', activeSubTab)
+spKeys.addEventListener('click', activeSubTab)
+customText.addEventListener('click', activeSubTab)
+
+alpha.val = 'alphabets'
+number.val = 'numbers'
+symbol.val = 'symbols'
+spKeys.val = 'specialKeys'
+customText.val = 'customText'
+
 
